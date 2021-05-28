afc856f32d345d2900396f10839c26dbe908d08f
janiellec <janielle@076HQ16167.frna.com>
2021-05-28 13:54:22 -0400

    Altered findvarinfile.py to use for entire finding process
    Initially, the process would have been:
    	1. Use Notepad++ to find all files in a directory that have $ (prime indicator of sysvar)
    	2. Check if those written down are correctly spelled using check_exist.cy
    	3. Change spelliing
    	4. Run the list of potential files with sysvars throuhg findvarinfile.py
    	5. Double check, filter, & edit sysvars
    
    But the process was cut down with this new version of findvarinfile.py where the process is shortened.
    
    Updates:
    	- main uses input of a folder under System Variable List (which is the only place that
    	  system variable folders will be found
    	- instead of reading a csv file with all the list of potential files, it only needs the directory
    	  and uses a similar code to check_exist.py where it goes through the directory using os.walk()
    	  and looks at each file and detects any "$" and if it does, it gets out of the file and reports the
    	  adds that file in a list. this is under the function sysfiles()
    	  (this process was tested and checked using Notepad++)
    	- added helper funciton search() helps sysfiles() search for "$"
    
    Because of these new upgrades, the process for searching system variable files is cut down to:
    	1. Find sysvar using findvarinfile.py
    	2. Double check, filter, & edit found sysvars
    
    Work is optimized and is ready for upcoming files. Very excited about these new developments.

--- findvarinfile/findvarinfile.py
+++ findvarinfile/findvarinfile.py
@@ -3,17 +3,67 @@ import re
 import os
 import csv
 
-def remove_white(string):
-    #removes whitespace and after
-    if var.find(' ') != -1:
-        return var[0:var.find(' ')]
-    else:  
-        return var
+# Finds variables in files provided using
+#       this format: $__something__<whatever demarcation used the file>
+# Does not take variables in error or comment functions (often duplicates & 
+#       and does not fit format). Removes whitespaces if those exist
+# Manual post-processing necessary to find special cases such as:
+#       1. '$__something__[' + variable + '].$__somelse__[' + number + ']
+#                         ^ premature       ^ searches for this second
+#                         termination       $ indication and demarcation
+#           - need to double check that every $....[ is closed off or
+#               includes the second part connected to it as opposed to separate
+#       2. '$__something__' + gunnumber + '[' + number + '].$__someelse__'
+#                         ^ premature termination
+#           - need to include gun number; not sure how many guns there are
 
+# Helper function: reads into a list and sees if it finds object
+# Returns bool of whether list of lines has character
+def search(list, character):
+    for line in list:
+        if character in line:
+            return True
+    return False
+
+# Finds files that might have system variables
+def sysfiles(directory):
+    path = '/mnt/c/Users/calaunjr/Desktop/FindSysVar/System Variable List/' + directory
+    list = []
+    countfiles, total = 0, 0
+    for root, dir, files in os.walk(path):
+        for file in files:
+            with open(path + '/' + file, 'r', encoding='latin-1') as one:
+                linelist = one.readlines()
+                total += 1
+                if search(linelist, '$'):
+                    list.append(file)
+                    countfiles += 1
+                    continue
+    print("Found " + str(countfiles) + " files with potential system variables out of " + str(total) + " files in " + directory)
+    return list
+
+# Determines what demarcation marks the end of a sysvar given the file type
+def fileext(name):
+    ext = name[name.find('.'):]
+    if ext == ".cm":
+        return " "
+    elif ext == ".xml" or ext == ".stm":
+        return "\""
+    else:
+        return "\'"
+
+# Finds spaces in string and removes what is from that space until the space
+def remove_white(string):
+    if string.find(' ') != -1:
+        return string[0:string.find(' ')-1]
+    else:  
+        return string
+
+# Searches for system variables according to pattern and stores it in an output file
 def find_basic(readfile, writefile, ext):
-    readfile = '/mnt/c/Users/calaunjr/Desktop/FindSysVar/System Variable List/v833155/' + readfile
+    readfile = '/mnt/c/Users/calaunjr/Desktop/FindSysVar/System Variable List/' + readfile
     pattern = re.compile('[$].+?(?=' + ext + ')')
-    with open(readfile, 'r') as t1:
+    with open(readfile, 'r', encoding='latin-1') as t1:
         for line in t1:
             if line.find("POST_ERR") != -1 or line.find("WriteLog") != -1:
                 continue
@@ -24,35 +74,20 @@ def find_basic(readfile, writefile, ext):
                     var = remove_white(var)
                     writefile.write(var + "\n")
 
-def fileext(name):
-    ext = name[name.find('.'):]
-    if ext == ".cm":
-        return " "
-    elif ext == ".xml" or ext == ".stm":
-        return "\""
-    else:
-        return "\'"
-
-
 if __name__ == '__main__':
 
-    listcheck = input("Enter list of all files to check: ")
+    filedir = input("Enter directory under System Variable List: ")
     output = input("Enter output file name: ")
-    with open(listcheck, 'r') as check, open(output, 'w+') as out:
-        list = check.read().splitlines()
-        for file in list:
+    
+    print('\n')
+    pot_files = sysfiles(filedir)
+    with open(output, 'w+', encoding='latin-1') as out:
+        for file in pot_files:
             ext = fileext(file)
-            find_basic(file, out, ext)
-
-
-
-
-
-
-
-
-
-
+            filepath = filedir + '/' + file
+            find_basic(filepath, out, ext)
+    print("""All found potential variables are in """ + output + 
+            """\nVariables are now ready for post-processing""")
 
 
 
