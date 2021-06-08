54a2c121610aab1f919a884c0a0217452130d284
janiellec <janielle@076HQ16167.frna.com>
2021-06-08 15:21:33 -0400

    LS file extention delimeter
    Uses a look forward with an or statement (e.g.: ?=\=|\s)

--- findvarinfile/findvarinfile.py
+++ findvarinfile/findvarinfile.py
@@ -27,7 +27,8 @@ def search(list, character):
 
 # Finds files that might have system variables
 def sysfiles(directory):
-    path = '/mnt/c/Users/calaunjr/Desktop/FindSysVar/System Variable List/' + directory
+    #path = '/mnt/c/Users/calaunjr/Desktop/FindSysVar/System Variable List/' + directory
+    path = '/mnt/c/Users/calaunjr/Desktop/FindSysVar/' + directory
     list = []
     countfiles, total = 0, 0
     for root, dir, files in os.walk(path):
@@ -45,41 +46,63 @@ def sysfiles(directory):
 # Determines what demarcation marks the end of a sysvar given the file type
 def fileext(name):
     ext = name[name.find('.'):]
-    if ext == ".cm":
-        return " "
+    if ext == ".kl":
+        return "\'"
+    elif ext == ".ls":
+        return "\s|\="
     elif ext == ".xml" or ext == ".stm":
         return "\""
+    elif ext == ".kls":
+        return "\s|\'"
     else:
-        return "\'"
+        return "\s"
 
 # Finds spaces in string and removes what is from that space until the space
-def remove_white(string):
+def remove_irrelevant(string):
+    if string.find('<') != -1:
+        string = string[0:string.find('<')]
+    if string.find('>') != -1:
+        string = string[0:string.find('>')]
+    if string.find('=') != -1:
+        string = string[0:string.find('=')]
+    if string.find(',') != -1:
+        string = string[0:string.find(',')]    
     if string.find(' ') != -1:
-        return string[0:string.find(' ')-1]
-    else:  
-        return string
+        string = string[0:string.find(' ')]
+    if string.find('\'') != -1:
+        string = string[0:string.find('\'')]
+    if string.find('\"') != -1:
+        string = string[0:string.find('\"')]
+    if string.find('\)') != -1:
+        string = string[0:string.find('\)')]
+    if string.find('\(') != -1:
+        string = string[0:string.find('\(')]
+    
+    return string
 
 # Searches for system variables according to pattern and stores it in an output file
 def find_basic(readfile, writefile, ext):
-    readfile = '/mnt/c/Users/calaunjr/Desktop/FindSysVar/System Variable List/' + readfile
+    #readfile = '/mnt/c/Users/calaunjr/Desktop/FindSysVar/System Variable List/' + readfile
+    readfile = '/mnt/c/Users/calaunjr/Desktop/FindSysVar/' + readfile
+    
     pattern = re.compile('[$].+?(?=' + ext + ')')
     with open(readfile, 'r', encoding='latin-1') as t1:
         for line in t1:
             if line.find("POST_ERR") != -1 or line.find("WriteLog") != -1:
                 continue
+            if line.find("$$") != -1:
+                continue
             else:
                 p = pattern.findall(line)
-            if p:
-                for var in p:
-                    var = remove_white(var)
-                    writefile.write(var + "\n")
+                if p:
+                    for var in p: 
+                        var = remove_irrelevant(var)
+                        writefile.write(var + "\n")
 
 if __name__ == '__main__':
 
     filedir = input("Enter directory under System Variable List: ")
     output = input("Enter output file name: ")
-    
-    print('\n')
     pot_files = sysfiles(filedir)
     with open(output, 'w+', encoding='latin-1') as out:
         for file in pot_files:
@@ -88,54 +111,3 @@ if __name__ == '__main__':
             find_basic(filepath, out, ext)
     print("""All found potential variables are in """ + output + 
             """\nVariables are now ready for post-processing""")
-
-
-
-
-
-
-#################################################################################################################
-#################################################################################################################
-################################# code that only partially works ################################################
-#################################################################################################################
-#####  MAIN ISSUE:  #############################################################################################
-#####       only reads the first variable in the line  ##########################################################
-#################################################################################################################
-#################################################################################################################
-#
-#def remove_white(string):
-#    #removes whitespace and after
-#    if var.find(' ') != -1:
-#        return var[0:var.find(' ')]
-#    else:  
-#        return var
-
-#def find_basic(readfile, writefile):
-#    pattern = re.compile('[$].+?(\[.\])??.+?(\[.\])??(?=\'|\")')
-#    #dirc = "/mnt/c/Users/calaunjr/Desktop/FindSysVar/System Variable List/v833155/"
-#    #readfile = dirc + readfile
-#    with open(readfile, 'r') as t1:
-#        for line in t1:
-#            if line.find("POST_ERR") != -1 or line.find("WriteLog") != -1:
-#                continue
-#            else:
-#                p = pattern.search(line)
-#            #if p != None:
-#            #    var = p.group()
-#            #    var = remove_white(var)
-#            #    if var not in writefile:
-#            #        writefile.write(var + "\n")
-
-#            if p != None:
-#                writefile.write(p.group() + "\n")
-
-#if __name__ == '__main__':
-   
-#    listcheck = input("Enter list of all files to check: ")
-#    output = input("Enter output file name: ")
-#    with open(listcheck, 'r') as check, open(output, 'w+') as out:
-#        list = check.read().splitlines()
-#        for file in list:
-#            # file += ".kl"
-#            find_basic(file, out)
-#    print("\n")
